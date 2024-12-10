7ee4747ff63000f2962b6959c1b6bfbc86fc61ab
tofayel <tofayeltuhin143@gmail.com>
2024-12-10 16:30:15 +0600

    add image in slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -13,50 +13,53 @@ import "./style.css";
 import { Pagination, Autoplay } from "swiper/modules";
 
 // Import images
-import img1 from "../../../public/HomeImg/Keralabig.webp";
-import img2 from "../../../public/HomeImg/MorochoBig.webp";
-import img3 from "../../../public/HomeImg/oatarbig.webp";
+import img1 from "../../../public/HomeImg/Keralabig.jpg";
+import img2 from "../../../public/HomeImg/moroccho.png";
+import img3 from "../../../public/HomeImg/qatarbig.jpg";
 import img4 from "../../../public/HomeImg/zangerBig.webp";
-import img5 from "../../../public/HomeImg/OmanBig.png"
+import img5 from "../../../public/HomeImg/omanbig.jpg"
+import img6 from "../../../public/HomeImg/malaisia.jpg"
 
 export default function CardSlider() {
   const cards = [
     { image: img5, country: "Oman", descriptions: "7 days from ", price: "$1475" },
-    { image: img3, country: "Kerala", descriptions: "7 days from ", price: "$1233" },
-    { image: img2, country: "Morocho", descriptions: "8 days from ", price: "$1133" },
-    { image: img1, country: "Qatar", descriptions: "2 days from ", price: "$1033" },
-    { image: img4, country: "Zanger", descriptions: "3 days from ", price: "$933" },
+    { image: img3, country: "Qatar", descriptions: "7 days from ", price: "$1475" },
+    { image: img6, country: "Malaysia", descriptions: "7 days from ", price: "$1475" },
+    { image: img2, country: "Morocco", descriptions: "8 days from ", price: "$1475" },
+    { image: img1, country: "Kerala", descriptions: "2 days from ", price: "$1475" },
+    { image: img4, country: "Zanzibar", descriptions: "3 days from ", price: "$1475" },
   ];
 
   return (
     <Swiper
-      slidesPerView={1.2}
-      spaceBetween={20}
-      pagination={{
-        clickable: true,
-      }}
-      autoplay={{
-        delay: 3000, // Delay between slides (3 seconds)
-        disableOnInteraction: false, // Autoplay continues after user interaction
-      }}
-      modules={[Pagination, Autoplay]} // Include the Autoplay module
-      className="mySwiper"
-    >
-      {cards.map((card, index) => (
-        <SwiperSlide key={index}>
-          <div className="card">
-            <img src={card.image} alt={card.country} />
-            <div className="card-text z-10 text-ivory-white text-center w-full p-4 shadow-md">
-              <h2 className="text-4xl font-bold text-ivory-white leading-10">
-                {card.country}
-              </h2>
-              <p className="text-xl font-normal text-ivory-white leading-10 uppercase">
-                {card.descriptions} <strong>{card.price}</strong>
-              </p>
-            </div>
+    slidesPerView="auto" 
+    spaceBetween={20} /* Control gap between cards */
+    pagination={{
+      clickable: true,
+    }}
+    autoplay={{
+      delay: 3000, // Delay between slides (3 seconds)
+      disableOnInteraction: false, // Autoplay continues after user interaction
+    }}
+    modules={[Pagination, Autoplay]} // Include the Autoplay module
+    className="mySwiper"
+  >
+    {cards.map((card, index) => (
+      <SwiperSlide key={index} style={{ width: "60%" }}> 
+        <div className="card">
+          <img src={card.image} alt={card.country} />
+          <div className="card-text z-10 text-ivory-white text-center w-full p-4 shadow-md">
+            <h2 className="text-4xl font-bold text-ivory-white leading-10">
+              {card.country}
+            </h2>
+            <p className="text-xl font-normal text-ivory-white leading-10 uppercase">
+              {card.descriptions} <strong>{card.price}</strong>
+            </p>
           </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+        </div>
+      </SwiperSlide>
+    ))}
+  </Swiper>
+  
   );
 }
