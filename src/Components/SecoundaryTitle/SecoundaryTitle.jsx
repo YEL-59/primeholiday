5cdf66162e10ebe880251ac16835a5602e5d8055
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-12-11 21:15:00 +0600

    destination page uselocation hook for data passing

--- src/Components/SecoundaryTitle/SecoundaryTitle.jsx
+++ src/Components/SecoundaryTitle/SecoundaryTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SecoundaryTitle = ({ head_title }) => {
   return (
-    <div className="text-primary max-w-xl bg-opacity-100 text-center md:text-start font-montserrat text-[25px] md:text-[40px] xl:text-[40px]  font-semibold md:leading-[60px]  md:tracking-[1.069px] uppercase">
+    <div className="text-primary max-w-7xl bg-opacity-100 text-center md:text-start font-montserrat text-[25px] md:text-[40px] xl:text-[38px]  font-semibold md:leading-[60px]  md:tracking-[1.069px] uppercase">
       <h1>{head_title}</h1>
     </div>
   )
--- src/Pages/DestinationDetails/DestinationComponent/DestinationHome_Section/DestinationHome_Section.jsx
+++ src/Pages/DestinationDetails/DestinationComponent/DestinationHome_Section/DestinationHome_Section.jsx
@@ -4,7 +4,7 @@ import bgImage from '../../../../../public/DestinationImg/Omanbg.png';
 const DestinationHome_Section = () => {
   const { destinationName } = useParams();
   const location = useLocation();
-  const { imgSrc } = location.state || {};
+  const { imgSrc,description } = location.state || {};
 
   return (
     <>
@@ -17,12 +17,12 @@ const DestinationHome_Section = () => {
         <div className="absolute inset-0 bg-gray-800 opacity-30 backdrop-blur-lg"></div>
         <div className="relative z-30 flex flex-col justify-around items-center w-full h-full">
           {/* Top Content */}
-          <div className="p-5 text-center mt-20">
+          <div className="p-5 text-center mt-28">
             <h1 className="text-5xl  md:text-5xl lg:text-[96px] font-salvager text-primary ">
               {destinationName}
             </h1>
-            <p className="text-xl  md:text-xl xl:text-6xl text-white mt-4">
-              Beauty Has an Address
+            <p className="text-xl  md:text-xl xl:text-6xl text-white mt-14 ">
+            {description || "Beauty Has an Address"}
             </p>
           </div>
 
